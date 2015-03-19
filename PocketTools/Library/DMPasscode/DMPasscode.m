4561d7405b574d2599f2820cacf3f4c551c5ac52
ycabc1989 <yc@ycdeMac.local>
2015-03-19 14:37:46 +0800

    修复可能crash的bug

--- PocketTools/Library/DMPasscode/DMPasscode.m
+++ PocketTools/Library/DMPasscode/DMPasscode.m
@@ -90,7 +90,7 @@ NSString * const DMUnlockErrorDomain = @"com.dmpasscode.error.unlock";
     _completion = completion;
     LAContext* context = [[LAContext alloc] init];
     if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:nil]) {
-        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:NSLocalizedString(@"使用指纹解锁", nil) reply:^(BOOL success, NSError* error) {
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"使用指纹解锁" reply:^(BOOL success, NSError* error) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 if (error) {
                     switch (error.code) {
@@ -143,9 +143,9 @@ NSString * const DMUnlockErrorDomain = @"com.dmpasscode.error.unlock";
     [nc setModalPresentationStyle:UIModalPresentationFormSheet];
     [viewController presentViewController:nc animated:YES completion:nil];
     if (_mode == 0) {
-        [_passcodeViewController setInstructions:NSLocalizedString(@"输入新密码", nil)];
+        [_passcodeViewController setInstructions:@"输入新密码"];
     } else if (_mode == 1) {
-        [_passcodeViewController setInstructions:NSLocalizedString(@"输入密码解锁", nil)];
+        [_passcodeViewController setInstructions:@"输入密码解锁"];
     }
 }
 
@@ -160,7 +160,7 @@ NSString * const DMUnlockErrorDomain = @"com.dmpasscode.error.unlock";
     if (_mode == 0) {
         if (_count == 0) {
             _prevCode = code;
-            [_passcodeViewController setInstructions:NSLocalizedString(@"请再次输入密码", nil)];
+            [_passcodeViewController setInstructions:@"请再次输入密码"];
             [_passcodeViewController setErrorMessage:@""];
             [_passcodeViewController reset];
         } else if (_count == 1) {
@@ -168,8 +168,8 @@ NSString * const DMUnlockErrorDomain = @"com.dmpasscode.error.unlock";
                 [[DMKeychain defaultKeychain] setObject:code forKey:KEYCHAIN_NAME];
                 [self closeAndNotify:YES withError:nil];
             } else {
-                [_passcodeViewController setInstructions:NSLocalizedString(@"输入新密码", nil)];
-                [_passcodeViewController setErrorMessage:NSLocalizedString(@"密码不对请再次输入", nil)];
+                [_passcodeViewController setInstructions:@"输入新密码"];
+                [_passcodeViewController setErrorMessage:@"密码不对请再次输入"];
                 [_passcodeViewController reset];
                 _count = 0;
                 return;
